6a6f5d5168110a90390e2176506cf29d69fef1c5
fseeeye <fseeeye@gmail.com>
2023-05-11 15:10:44 +0800

    refactor: make homework1 more formal

--- homework/homework1/README.md
+++ homework/homework1/README.md
@@ -1,41 +1,56 @@
 # GAMES 106 - 作业1
 
-## 作业要求
+## 作业要求与基本情况
 
+作业要求：
 - 支持gltf的骨骼动画。
-- 支持gltf的PBR的材质，包括法线贴图。
+- 支持gltf的PBR的材质，包括法线贴图等。
 - 进阶作业：
-    - 增加一个Tone Mapping的后处理 Render Pass。tonemap选择ACES实现如下:
-        ``` C++
-        // tonemap 所使用的函数
-        float3 Tonemap_ACES(const float3 c) {
-            // Narkowicz 2015, "ACES Filmic Tone Mapping Curve"
-            // const float a = 2.51;
-            // const float b = 0.03;
-            // const float c = 2.43;
-            // const float d = 0.59;
-            // const float e = 0.14;
-            // return saturate((x*(a*x+b))/(x*(c*x+d)+e));
+	- 增加一个Tone Mapping的后处理 Render Pass，用于线性颜色的转换。tonemap选择ACES实现如下:
+		``` C++
+		// tonemap 所使用的函数
+		float3 Tonemap_ACES(const float3 c) {
+			// Narkowicz 2015, "ACES Filmic Tone Mapping Curve"
+			// const float a = 2.51;
+			// const float b = 0.03;
+			// const float c = 2.43;
+			// const float d = 0.59;
+			// const float e = 0.14;
+			// return saturate((x*(a*x+b))/(x*(c*x+d)+e));
 
-            //ACES RRT/ODT curve fit courtesy of Stephen Hill
-	        float3 a = c * (c + 0.0245786) - 0.000090537;
-	        float3 b = c * (0.983729 * c + 0.4329510) + 0.238081;
-	        return a / b;
-        }
-        ```
-    - 增加GLTF的滤镜功能。
+			//ACES RRT/ODT curve fit courtesy of Stephen Hill
+			float3 a = c * (c + 0.0245786) - 0.000090537;
+			float3 b = c * (0.983729 * c + 0.4329510) + 0.238081;
+			return a / b;
+		}
+		```
+	- 增加GLTF的滤镜功能。
+
+作业初始状态：
+- 只是读取了 GLTF 文件，但是是静态的，并没有读取骨骼动画。
+- BRDF 实现存在问题，仅将颜色作为材质且仅有一个方向光，没有实现 PBR 材质。
+
+## TODO List
+
+- [ ] 载入 GLTF 缺失的数据。
+	- [ ] 载入骨骼和动画数据。
+	- [ ] 读取纹理贴图(Texture mapping)：法线、字符光、PBR。
+- [ ] 补充 GLTF Render Pass。
+	- [ ] Vertex Shader 中支持骨骼动画计算。
+	- [ ] Fragment Shader 实现 PBR 的直接光照和间接光照。
+- [ ] 新增 Tonemap Render Pass，用于线性颜色的转换。
 
 ## 说明
 
-- 编译 HLSL shader：
-    - Fragment Shader : `dxc -E main <path\to\shader> -T ps_6_0 -spirv -Fo <path\to\spirv\output>`
-    - Vertex Shader : `dxc -E main <path\to\shader> -T vs_6_0 -spirv -Fo <path\to\spirv\output>`
-- 编译 GLSL shader: `glslangValidator -V <path\to\shader> -o <path\to\spirv\output>`
+- 编译 shader 至 SPRIV 的命令：
+	- HLSL Fragment Shader : `dxc -E main <path\to\shader> -T ps_6_0 -spirv -Fo <path\to\spirv\output>`
+	- HLSL Vertex Shader : `dxc -E main <path\to\shader> -T vs_6_0 -spirv -Fo <path\to\spirv\output>`
+	- GLSL shader: `glslangValidator -V <path\to\shader> -o <path\to\spirv\output>`
 
 ## Ref
 
 - GTLF 文档: https://github.com/KhronosGroup/glTF
 - 骨骼动画示例：[examples/gltfskinning/](https://github.com/SaschaWillems/Vulkan/tree/master/examples/gltfskinning)
 - PBR 材质示例
-    - 直接光照: [examples/pbrbasic/](https://github.com/SaschaWillems/Vulkan/tree/master/examples/pbrbasic)
-    - 环境光照: [examples/pbribl/](https://github.com/SaschaWillems/Vulkan/tree/master/examples/pbribl)
+	- 直接光照: [examples/pbrbasic/](https://github.com/SaschaWillems/Vulkan/tree/master/examples/pbrbasic)
+	- 环境光照: [examples/pbribl/](https://github.com/SaschaWillems/Vulkan/tree/master/examples/pbribl)
---
+++ homework/homework1/homework1.h
@@ -0,0 +1,216 @@
+#pragma once
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#ifdef VK_USE_PLATFORM_ANDROID_KHR
+#define TINYGLTF_ANDROID_LOAD_FROM_ASSETS
+#endif
+#include "tiny_gltf.h"
+
+#include "vulkanexamplebase.h"
+
+#define ENABLE_VALIDATION false
+
+// Contains everything required to render a glTF model in Vulkan
+// This class is heavily simplified (compared to glTF's feature set) but retains the basic glTF structure
+class VulkanglTFModel
+{
+public:
+	// The class requires some Vulkan objects so it can create it's own resources
+	vks::VulkanDevice* vulkanDevice;
+	VkQueue copyQueue;
+
+	/*
+		Base glTF structures, see gltfscene sample for details
+	*/
+
+	// The vertex layout for the samples' model
+	struct Vertex {
+		glm::vec3 pos;
+		glm::vec3 normal;
+		glm::vec2 uv;
+		glm::vec3 color;
+	};
+
+	// Single vertex buffer for all primitives
+	struct {
+		VkBuffer buffer;
+		VkDeviceMemory memory;
+	} vertices;
+
+	// Single index buffer for all primitives
+	struct {
+		int count;
+		VkBuffer buffer;
+		VkDeviceMemory memory;
+	} indices;
+
+	// The following structures roughly represent the glTF scene structure
+	// To keep things simple, they only contain those properties that are required for this sample
+
+	// A primitive contains the data for a single draw call
+	struct Primitive {
+		uint32_t firstIndex;
+		uint32_t indexCount;
+		int32_t materialIndex;
+	};
+
+	// Contains the node's (optional) geometry and can be made up of an arbitrary number of primitives
+	struct Mesh {
+		std::vector<Primitive> primitives;
+	};
+
+	// A node represents an object in the glTF scene graph
+	struct Node {
+		Node* parent;
+		std::vector<Node*> children;
+		Mesh mesh;
+		glm::mat4 matrix;
+		~Node() {
+			for (auto& child : children) {
+				delete child;
+			}
+		}
+	};
+
+	// A glTF material stores information in e.g. the texture that is attached to it and colors
+	struct Material {
+		glm::vec4 baseColorFactor = glm::vec4(1.0f);
+		uint32_t baseColorTextureIndex;
+	};
+
+	// Contains the texture for a single glTF image
+	// Images may be reused by texture objects and are as such separated
+	struct Image {
+		vks::Texture2D texture;
+		// We also store (and create) a descriptor set that's used to access this texture from the fragment shader
+		VkDescriptorSet descriptorSet;
+	};
+
+	// A glTF texture stores a reference to the image and a sampler
+	// In this sample, we are only interested in the image
+	struct Texture {
+		int32_t imageIndex;
+	};
+
+	/*
+		Skin structure
+	*/
+
+	struct Skin
+	{
+		std::string            name;
+		Node* skeletonRoot = nullptr;
+		std::vector<glm::mat4> inverseBindMatrices;
+		std::vector<Node*>    joints;
+		vks::Buffer            ssbo;
+		VkDescriptorSet        descriptorSet;
+	};
+
+	/*
+		Animation related structures
+	*/
+
+	struct AnimationSampler
+	{
+		std::string            interpolation;
+		std::vector<float>     inputs;
+		std::vector<glm::vec4> outputsVec4;
+	};
+
+	struct AnimationChannel
+	{
+		std::string path;
+		Node* node;
+		uint32_t    samplerIndex;
+	};
+
+	struct Animation
+	{
+		std::string                   name;
+		std::vector<AnimationSampler> samplers;
+		std::vector<AnimationChannel> channels;
+		float                         start = std::numeric_limits<float>::max();
+		float                         end = std::numeric_limits<float>::min();
+		float                         currentTime = 0.0f;
+	};
+
+	/*
+		Model data
+	*/
+	std::vector<Image> images;
+	std::vector<Texture> textures;
+	std::vector<Material> materials;
+	std::vector<Node*> nodes;
+
+	uint32_t activeAnimation = 0;
+
+public:
+	~VulkanglTFModel();
+
+	void loadImages(tinygltf::Model& input);
+	void loadTextures(tinygltf::Model& input);
+	void loadMaterials(tinygltf::Model& input);
+	void loadNode(const tinygltf::Node& inputNode, const tinygltf::Model& input, VulkanglTFModel::Node* parent, std::vector<uint32_t>& indexBuffer, std::vector<VulkanglTFModel::Vertex>& vertexBuffer);
+	
+	void drawNode(VkCommandBuffer commandBuffer, VkPipelineLayout pipelineLayout, VulkanglTFModel::Node* node);
+	void draw(VkCommandBuffer commandBuffer, VkPipelineLayout pipelineLayout);
+};
+
+
+class VulkanExample final : public VulkanExampleBase
+{
+public:
+	bool wireframe = false;
+
+	VulkanglTFModel glTFModel;
+
+	struct ShaderData {
+		vks::Buffer buffer;
+		struct Values {
+			glm::mat4 projection;
+			glm::mat4 model;
+			glm::vec4 lightPos = glm::vec4(5.0f, 5.0f, -5.0f, 1.0f);
+			glm::vec4 viewPos;
+		} values;
+	} shaderData;
+
+	struct Pipelines {
+		VkPipeline solid = VK_NULL_HANDLE;
+		VkPipeline wireframe = VK_NULL_HANDLE;
+	} pipelines;
+
+	VkPipelineLayout pipelineLayout;
+	VkDescriptorSet descriptorSet;
+
+	struct DescriptorSetLayouts {
+		VkDescriptorSetLayout matrices;
+		VkDescriptorSetLayout textures;
+	} descriptorSetLayouts;
+
+public:
+	VulkanExample();
+	~VulkanExample() override;
+
+	virtual void getEnabledFeatures() override;
+	virtual void buildCommandBuffers() override;
+	virtual void OnUpdateUIOverlay(vks::UIOverlay* overlay) override;
+
+	void loadglTFFile(std::string filename);
+	void loadAssets();
+	void prepareUniformBuffers();
+	void setupDescriptors();
+	void preparePipelines();
+	/**
+	 * @brief Prepares all Vulkan resources and functions required to run the sample
+	 */
+	virtual void prepare() override;
+
+	void updateUniformBuffers();
+	/**
+	 * @brief (Pure virtual) Render function to be implemented by the sample application
+	 */
+	virtual void render() override;
+	virtual void viewChanged() override;
+};
